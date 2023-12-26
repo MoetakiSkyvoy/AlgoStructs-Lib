7707e4ed99bde51980c195072e8d3da74626b472
Moetaki <skyvoy@moetaki.com>
2023-12-26 16:30:26 +0800

    Add files via upload
    The basic header file of this project.
    本项目的基础头文件。

---
+++ stdblibal.h
@@ -0,0 +1,17 @@
+// stdblibal.h - Standard Base Library for AlgoStructs-Lib
+// ----- This header file is used for calls to the AlgoStructs-Lib project. -----
+// ----- 本头文件适用于AlgoStructs-Lib项目的调用。 -----
+
+// basic library calls
+// 基本库调用
+#include <stdio.h>
+#include <stdlib.h>
+
+// function result/status code
+// 函数结果状态代码
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
