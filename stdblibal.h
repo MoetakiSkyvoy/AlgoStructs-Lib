ae3d6e7235e7aff731f451ad4333e6f6c422e752
萌瀧·天遊 <skyvoy@moetaki.com>
2023-12-26 19:46:34 +0800

    Update stdblibal.h
    Standardized the format of English comments.
    规范了英文注释的格式。

--- stdblibal.h
+++ stdblibal.h
@@ -2,12 +2,12 @@
 // ----- This header file is used for calls to the AlgoStructs-Lib project. -----
 // ----- 本头文件适用于AlgoStructs-Lib项目的调用。 -----
 
-// basic library calls
+// Basic library calls
 // 基本库调用
 #include <stdio.h>
 #include <stdlib.h>
 
-// function result/status code
+// Function result/status code
 // 函数结果状态代码
 #define OK 1
 #define ERROR 0
